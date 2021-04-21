d62c122ce3cf2d750ff880162957cb7d980d9065
Cihangir Ozmus <cozmus@Cihangirs-MacBook-Pro.local>
2021-04-21 20:56:14 +0300

    react-videos is added

--- react-videos/src/components/VideoList.js
+++ react-videos/src/components/VideoList.js
@@ -1,7 +1,11 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem video={video} key={video.id.videoId} onVideoSelect={onVideoSelect}/>;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
